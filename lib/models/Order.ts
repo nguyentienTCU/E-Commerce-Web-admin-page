9b41ce672e91e5fbac059b9004bf6c0980f16610
tiennguyenTCU <nguyentien3036@gmail.com>
2025-03-31 01:30:16 -0500

    feat: create api for saving orders and customers who have orders in the database

---
+++ lib/models/Order.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  customerClerkId: String,
+  products: [
+    {
+      productTitle: String,
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      color: String,
+      size: String,
+      quantity: Number,
+    },
+  ],
+  shippingAddress: {
+    streetAddress1: String,
+    streetAddress2: String,
+    city: String,
+    state: String,
+    postalCode: String,
+    country: String,
+  },
+  shippingRate: String,
+  totalAmount: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+
+export default Order;
